75bf418b41a98dfe2e06dd01dcd37e0db3b69b1a
Diego Oliveira <diegosetubal10@hotmail.com>
2019-05-08 01:42:04 -0300

    Removing express default routers

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import indexRouter from './routes/index';
-import usersRouter from './routes/users';
 import consultaRouter from './routes/consulta';
 
 const app = express();
@@ -15,7 +14,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/consulta', consultaRouter);
 
 module.exports = app;
